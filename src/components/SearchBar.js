173fe157aa7a8a464dd3122c5f6f0eff57b00f0e
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-11 21:19:20 -0700

    added component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+function SearchBar({ query, setQuery }) {
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleChange = async (e) => {
+    const val = e.target.value;
+    setQuery(val);
+
+    if (val.length > 2) {
+      const res = await fetch(`https://www.omdbapi.com/?apikey=YOUR_OMDB_API_KEY&s=${val}&page=1`);
+      const data = await res.json();
+      if (data.Response === 'True') {
+        setSuggestions(data.Search.slice(0, 5));
+      } else {
+        setSuggestions([]);
+      }
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Search for movies..."
+        value={query}
+        onChange={handleChange}
+      />
+      {suggestions.length > 0 && (
+        <ul className="suggestions">
+          {suggestions.map((item) => (
+            <li key={item.imdbID} onClick={() => setQuery(item.Title)}>
+              {item.Title} ({item.Year})
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default SearchBar;
